b571bf03851218b7a09b1575bacbd56b0a9c342a
Danya <dertushev@mail.ru>
2025-02-07 18:34:25 +0400

    delete comments

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 
 namespace Lab1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         Dictionary<char, string> utf8ToKoi8rDictionary = new Dictionary<char, string>
